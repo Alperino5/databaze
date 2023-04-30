a6422dc1d3d4fff54db78a337e77688e1de5947a
Alper <75456808+ItsAlper@users.noreply.github.com>
2023-04-30 13:28:35 +0200

    update vysvětlení

--- plang/script2.sql
+++ plang/script2.sql
@@ -1,10 +1,12 @@
 DROP PROCEDURE IF EXISTS update_stock_count;
 DELIMITER //
 CREATE PROCEDURE update_stock_count
+    /* "book_id" a "sold_count" jsou 2 vstupní údaje */
     (
         IN book_id INT,
         IN sold_count INT
     )
+    /* "UPDATE" aktualizuje hodnotu sloupce "stock_count" v tabulce "plang.books" */
     BEGIN
         UPDATE plang.books
         SET stock_count = stock_count - sold_count
