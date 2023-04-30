d341c4ecf5111379b7f9de1beeb660d93f673936
Alper <75456808+ItsAlper@users.noreply.github.com>
2023-04-30 13:28:17 +0200

    update vysvětlení

--- plang/script.sql
+++ plang/script.sql
@@ -1,9 +1,11 @@
 DROP PROCEDURE IF EXISTS calculate_book_rating_avg;
 DELIMITER //
 CREATE PROCEDURE calculate_book_rating_avg
+    /* Jeden vstupní parametr  "book_id" */
     (
         IN book_id INT
     )
+    /* "SELECT" s funkcí "AVG" spočítá průměrnou hodnotu z hodnocení knihy v tabulce "plang.book_ratings" */
     BEGIN
         SELECT AVG(rating) AS avg_rating
         FROM plang.book_ratings
